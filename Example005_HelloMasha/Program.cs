5f3f0be2ed7fc700bad65a6204d19c57ba8e2704
Aslan <at_aslan_07@mail.ru>
2022-12-15 17:22:07 +0300

    Example005 HelloMasha

---
+++ Example005_HelloMasha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine ();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Оооо Маш, давно не виделись!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
