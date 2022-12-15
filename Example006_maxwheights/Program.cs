83b7ca4f1f7640e81add4c31df254e9cedb928fd
Aslan <at_aslan_07@mail.ru>
2022-12-15 17:37:30 +0300

    Example006 Wheights

---
+++ Example006_maxwheights/Program.cs
@@ -0,0 +1,13 @@
+﻿int A = 1;
+int B = 2;
+int C = 3;
+int D = 4;
+
+int MAX = A;
+if(A > MAX) MAX = A;
+if(B > MAX) MAX = B;
+if(C > MAX) MAX = C;
+if(D > MAX) MAX = D;
+
+Console.Write("MAX = ");
+Console.WriteLine(MAX);
